e790ab4f8b787d74cc36a08ec6c8a2422637d256
Giselle Garcia <gisellepagarcia@gmail.com>
2025-11-24 15:12:15 -0300

    Fix: Funcionamento do App com as Rotas

--- src/components/Introducao/introducaoData.ts
+++ src/components/Introducao/introducaoData.ts
@@ -1,5 +1,10 @@
 import { SlideData } from './types'; 
 import { ImageSourcePropType } from 'react-native';
+import MascoteComCapa from '../../assets/Mascote/MascoteComCapa.png';
+import MascoteOQueFazer from '../../assets/Mascote/MascoteOQueFazer.png';
+import MascoteDoando from '../../assets/Mascote/MascoteDoando.png';
+import MascoteRegistro from '../../assets/Mascote/MascoteRegistro.png';
+import MascoteComunidade from '../../assets/Mascote/MascoteComunidade.png';
 
 
 export const introSlides: SlideData[] = [
@@ -7,35 +12,35 @@ export const introSlides: SlideData[] = [
         id: 0,
         title: 'Olá!',
         description: 'Que bom te ver aqui! Pronto para doar vida?',
-        image: require('../../assets/mascote/MascoteComCapa.png') as ImageSourcePropType,
+        image: MascoteComCapa as ImageSourcePropType,
         buttonText: 'Iniciar',
     },
     {
         id: 1,
         title: 'O que Fazer Antes de Doar',
         description: 'Te ajudamos na preparação para doar sangue com um passo a passo.',
-        image: require('../../assets/mascote/MascoteOQueFazer.png') as ImageSourcePropType,
+        image: MascoteOQueFazer as ImageSourcePropType,
         buttonText: 'Arraste para o lado ➠ (1/4)',
     },
     {
         id: 2,
         title: 'Questionário',
         description: 'Responda a perguntas rápidas para verificar sua aptidão para doação.',
-        image: require('../../assets/mascote/MascoteDoando.png') as ImageSourcePropType,
+        image: MascoteDoando as ImageSourcePropType,
         buttonText: 'Próximo (2/4)',
     },
     {
         id: 3,
         title: 'Registro de Doações',
         description: 'Acompanhe seu histórico e descubra quando estará elegível para doar novamente.',
-        image: require('../../assets/mascote/MascoteRegistro.png') as ImageSourcePropType, 
+        image: MascoteRegistro as ImageSourcePropType, 
         buttonText: 'Próximo (3/4)',
     },
     {
         id: 4,
         title: 'Comunidade',
         description: 'Conecte-se com amigos doadores e encontre locais de doação perto de você.',
-        image: require('../../assets/mascote/MascoteComunidade.png') as ImageSourcePropType,
+        image: MascoteComunidade as ImageSourcePropType,
         buttonText: 'Fechar Tour',
     },
 ];
